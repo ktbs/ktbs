19f402a5f36473aaaf3982bcfa0e08527145568d
vincent <vincent-octo@users.noreply.github.com>
2014-04-15 09:41:47 +0200

    Add a nosetest showing concurrency problem
    When calling base.create_model() we have the following call stack:
    
    ```
    -> base.create_model()
      -> base.post_graph() ** Gets a semaphore for BASE, bringing semaphore value to 0
        -> mixins.post_graph()
          -> base.ack_post()
            -> base.edit() ** Gets a semaphore for BASE, waiting for upper semaphore to release
    ```
    
    We see that a semaphore for BASE is needed multiple times in these encapsulated call. Making it impossible to proceed.

--- utest/test_ktbs_engine_base_locking.py
+++ utest/test_ktbs_engine_base_locking.py
@@ -43,6 +43,7 @@ class TestKtbsBaseLocking(KtbsTestCase):
 
         # Tries to get a semaphore and delete a base,
         # it should block on the acquire() and raise a BusyError because a timeout is set.
+        # NOTE this takes several seconds (default timeout) to do.
         with assert_raises(posix_ipc.BusyError):
             base = Base(self.service, new_base.uri)
             base.delete()
@@ -50,3 +51,31 @@ class TestKtbsBaseLocking(KtbsTestCase):
         # Finally closing the semaphore we created for testing purpose.
         semaphore.release()
         semaphore.close()
+
+
+class KtbsTraceTestCase(KtbsTestCase):
+
+    base = None
+    model = None
+    trace = None
+
+    def setUp(self):
+        super(KtbsTraceTestCase, self).setUp()
+        self.base = self.my_ktbs.create_base()
+        self.model = self.base.create_model()
+        self.trace = self.base.create_stored_trace(None, self.model)
+
+    def tearDown(self):
+        super(KtbsTraceTestCase, self).tearDown()
+        self.base = None
+        self.model = None
+        self.trace = None
+
+
+class TestKtbsInBaseLocking(KtbsTraceTestCase):
+    """Test InBase locking in a kTBS context."""
+
+    def test_delete_trace(self):
+        with assert_raises(posix_ipc.BusyError):
+            with get_lock(self.base.get_uri(), timeout=5):
+                self.trace.delete()
