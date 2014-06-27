e418ffaf7e9a0e51f51572a887d444c91ec4de89
Vincent <vincent-octo@users.noreply.github.com>
2014-06-27 16:16:15 +0200

    Add doc for create() and reset_lock() edge cases

--- lib/ktbs/engine/lock.py
+++ lib/ktbs/engine/lock.py
@@ -39,6 +39,23 @@ def reset_lock(resource_uri):
     """Reset the value of the semaphore to 1 for a given resource.
 
     :param basestring resource_uri: the URI of the resource that is possibly locked.
+
+    .. note ::
+
+        In some edge cases, this function can put the kTBS in a bad state. For example,
+        the default lock value for a resource could be left to 2, removing the thread safety
+        entirely for this resource.
+
+        This could happen with the following scenario: a resource acquires a lock by
+        calling :meth:`WithLockMixin.lock`, bringing the lock value to 0.
+        In the same time, this function, :func:`reset_lock`, is called: it checks that
+        the lock value is 0 and then bring it to 1.
+        Then, the call to :meth:`WithLockMixin.lock` ends, releasing the lock:
+        it brings the lock value from 1 to 2.
+
+        As a result of having the lock value to 2, two calls to a method that needs a lock
+        are not going to be run one after another. They will run at the same time, which
+        defeats the purpose of using lock. This could lead to inconsistencies in the kTBS state.
     """
     semaphore = posix_ipc.Semaphore(name=get_semaphore_name(resource_uri),
                                     flags=posix_ipc.O_CREAT)
@@ -146,5 +163,9 @@ class WithLockMixin(object):
 
     @classmethod
     def create(cls, service, uri, new_graph):
+        """ I am called when a :class:`KtbsRoot` or a :class:`Base` is created.
+        After checking that the resource we create is correct, I set its lock to 1 because it is new
+        resource so there is no reason for it to be locked.
+        """
         super(WithLockMixin, cls).create(service, uri, new_graph)
         reset_lock(uri)
