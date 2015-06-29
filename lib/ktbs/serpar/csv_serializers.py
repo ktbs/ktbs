5fc3c1388351137c72cb1723cdddd4295f63fb80
Françoise Conil <fcodvpt@gmail.com>
2015-06-29 17:04:16 +0200

    Very first obsel CSV serializer

--- lib/ktbs/serpar/csv_serializers.py
+++ lib/ktbs/serpar/csv_serializers.py
@@ -18,11 +18,13 @@
 """
 I provide kTBS CSV serializer, this is a serialization with information loss.
 """
+from string import maketrans
+
 from rdflib import BNode, Literal, RDF, RDFS, URIRef, XSD
 from rdfrest.serializers import register_serializer, SerializeError
 from rdfrest.util import coerce_to_uri, wrap_exceptions
 
-from jsonld_serializers import ValueConverter, encode_unicodes
+from jsonld_serializers import ValueConverter
 
 from ..namespace import KTBS, KTBS_NS_URI
 from ..utils import SKOS
@@ -31,11 +33,9 @@ LEN_KTBS = len(KTBS_NS_URI)+1
 
 CSV = "text/csv"
 
-
 @register_serializer(CSV, "csv", 85, KTBS.ComputedTraceObsels)
 @register_serializer(CSV, "csv", 85, KTBS.StoredTraceObsels)
 @wrap_exceptions(SerializeError)
-@encode_unicodes
 def serialize_csv_trace_obsels(graph, tobsels, bindings=None):
 
     trace_uri = tobsels.trace.uri
@@ -49,10 +49,53 @@ def serialize_csv_trace_obsels(graph, tobsels, bindings=None):
         PREFIX : <%s#>
         SELECT DISTINCT ?attr
         {
-            $obs :hasTrace [] ; ?attr [].
+            ?obs :hasTrace [] ; ?attr [].
         }
     """ % KTBS_NS_URI)
 
-    attr_list = [valconv_uri(attr[0]) for attr in obsels]
-    s = u";".join(attr_list)
-    yield u"{0}\n".format(s)
+    # Build meaningfull sparql variables without special chars
+    all_attr = {}
+    intab = u'#:'
+    outtab = u'_'
+    # maketrans(intab, outtab) does not work with unicode
+    transtab = dict((ord(char), u'_') for char in intab)
+    for attr in obsels:
+        abr = None
+
+        # TODO do a clean thing for type
+        if attr[0].find("22-rdf-syntax-ns#type") != -1:
+            abr = "type"
+        else:
+            # TODO Dirty hack to remove hasTrace attribute : improve
+            if attr[0].find("ktbs#hasTrace") == - 1:
+                abr = valconv_uri(attr[0]).translate(transtab)
+
+        if abr is not None:
+            if all_attr.get(abr) is None:
+                all_attr[abr] = attr[0]
+
+            # TODO : at least a warning if we have several variables with
+            # the same name
+            #else:
+            #    raise Something
+        
+        #yield u"{0} : {1}\n".format(abr, attr[0]).encode('utf-8)
+
+    opt_attr = []
+    for abr, attr in all_attr.items():
+        opt_attr.append(u'OPTIONAL {{ ?id <{0}> ?{1} }}'.format(attr, abr))
+
+    all_opts = u'\n\t'.join(opt_attr)
+
+    header = u' '.join(['?{0}'.format(abr) for abr in all_attr.keys()])
+
+    obs_query = u"""SELECT {0}
+            {{
+                ?id <http://liris.cnrs.fr/silex/2009/ktbs#hasTrace> [] .
+                  {1}
+            }} ORDER BY ?hasEnd ?hasBegin""".format(header, all_opts)
+
+    obsels = graph.query(obs_query)
+
+    s = obsels.serialize(format='csv', encoding='utf-8')
+    yield s
