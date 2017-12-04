83ca2bbb0ad5eeccb70ac002420bcb2b825eaeca
Pierre-Antoine Champin <pchampin@liris.cnrs.fr>
2017-12-04 17:10:57 +0100

    ktbs: can now build a standalone ktbs command in Linux

---
+++ build_exe.sh
@@ -0,0 +1,20 @@
+#!/bin/sh
+mkdir -p buid
+mkdir -p dist
+echo "*" >build/.gitignore
+echo "*" >dist/.gitignore
+exec pyinstaller bin/ktbs \
+    --hidden-import rdflib.plugins.memory \
+    --hidden-import rdflib.plugins.sleepycat \
+    --hidden-import rdflib.plugins.parsers.notation3 \
+    --hidden-import rdflib.plugins.parsers.nt \
+    --hidden-import rdflib.plugins.serializers.n3 \
+    --hidden-import rdflib.plugins.serializers.nt \
+    --hidden-import rdflib.plugins.serializers.rdfxml \
+    --hidden-import rdflib.plugins.serializers.turtle \
+    --hidden-import ktbs.plugins.cors \
+    --hidden-import ktbs.plugins.meth_external \
+    --hidden-import ktbs.plugins.profiler \
+    --hidden-import ktbs.plugins.sparql_endpoints \
+    --hidden-import ktbs.plugins.xgereco_obsel_table \
+    -F
